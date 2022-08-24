21ac399d54efbbedf5b0cd7866f40c64871e8d08
jem-thanmay <86341905+jem-thanmay@users.noreply.github.com>
2022-08-24 15:22:34 +0530

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Assignment
+    {
+        public static void Main()
+        {
+            int a, b, c;
+
+            Console.Write("Enter the 1st number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the 2nd number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the 3rd number: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            int result = a * b * c;
+            Console.WriteLine(result);
+        }
+    }
+}
